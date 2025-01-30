ae16e590c22bf76ca9689c5bedf1b99ecccc7f0a
itelet <szabo.adam.0725@gmail.com>
2025-01-30 23:32:13 +0100

    Add images, rewrite gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,46 +1,99 @@
-import ImageCard from "./ImageCard";
-import Image1 from '../../assets/images/gallery/1.png'
+import GalleryCard from "./GalleryCard";
 
+import ST1 from "../../assets/images/gallery/solidtaste/1.png";
+import ST2 from "../../assets/images/gallery/solidtaste/2.png";
+import ST3 from "../../assets/images/gallery/solidtaste/3.png";
+import ST4 from "../../assets/images/gallery/solidtaste/4.png";
+import ST5 from "../../assets/images/gallery/solidtaste/5.png";
+import ST6 from "../../assets/images/gallery/solidtaste/6.png";
+import ST7 from "../../assets/images/gallery/solidtaste/7.png";
+import ST8 from "../../assets/images/gallery/solidtaste/8.png";
+import ST9 from "../../assets/images/gallery/solidtaste/9.png";
+
+import GD1 from "../../assets/images/gallery/gd/1.jpg";
+import GD2 from "../../assets/images/gallery/gd/2.jpg";
+import GD3 from "../../assets/images/gallery/gd/3.jpg";
+import GD4 from "../../assets/images/gallery/gd/4.jpg";
 
 export const Gallery = () => {
-  // Sample images
-  const images = [
+  const galleryData = [
     {
-      src: Image1,
-      title: "Image 1",
-      text: "Description 1",
+      title: "Solid Taste",
+      images: [
+        {
+          src: ST1,
+          description: "Recipes screen",
+        },
+        {
+          src: ST2,
+          description: "Single recipe screen",
+        },
+        {
+          src: ST3,
+          description: "Meal planner screen",
+        },
+        {
+          src: ST4,
+          description: "Cooking mode screen",
+        },
+        {
+          src: ST5,
+          description: "Shopping list screen",
+        },
+        {
+          src: ST6,
+          description: "Shopping list details",
+        },
+        {
+          src: ST7,
+          description: "Settings screen",
+        },
+        {
+          src: ST8,
+          description: "Dark mode recipes screen",
+        },
+        {
+          src: ST9,
+          description: "Dark mode search screen",
+        },
+      ],
     },
     {
-      src: "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
-      title: "Image 2",
-      text: "Description 2",
-    },
-    {
-      src: "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
-      title: "Image 3",
-      text: "Description 3",
-    },
-    {
-      src: "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
-      title: "Image 4",
-      text: "Description 4",
+      title: "Gamma Digital Kft.",
+      images: [
+        {
+          src: GD1,
+          description: "Crane warehouse structure",
+        },
+        {
+          src: GD2,
+          description: "Crane warehouse structure",
+        },
+        {
+          src: GD3,
+          description: "AGV (Automated Guided Vehicle)",
+        },
+        {
+          src: GD4,
+          description: "AGV in action",
+        },
+      ],
     },
   ];
 
   return (
-    <div className="mt-8 h-full w-full flex flex-col items-center bg-[#1E1E1E]">
+    <div className="mt-8 pb-8 h-full w-full flex flex-col items-center bg-[#1E1E1E]">
       <div className="mt-4 mb-3">
         <p className="text-[#70C368] font-[Inconsolata] font-extrabold text-3xl">
           GALLERY
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-        {images.map((img, index) => (
-          <ImageCard
+      <div className="container mt-4 d-flex flex-wrap justify-content-center">
+        {galleryData.map((gallery, index) => (
+          <GalleryCard
             key={index}
-            imageSrc={img.src}
-            title={img.title}
-            text={img.text}
+            title={gallery.title}
+            images={gallery.images}
           />
         ))}
       </div>
---
+++ src/components/Gallery/GalleryCard.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Card, Modal, Carousel } from "react-bootstrap";
+
+import ClickIcon from "../../assets/images/click_icon.png";
+
+/*
+TODO rewrite - this whole css cinema is stupid 
+-> have to find out stupid bootstrap classes for <Modal /> and <Carousel />
+*/
+const GalleryCard = ({ title, images }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Card
+        className="m-2 w-72 cursor-pointer bg-[#1e1e1e] relative"
+        style={{ border: "none" }}
+        onClick={() => setShowModal(true)}
+      >
+        <div className="relative">
+          <Card.Img
+            variant="top"
+            src={images[0].src}
+            alt={images[0].description}
+            className="border-none w-full h-48 object-cover"
+          />
+          <div className="absolute bottom-2 right-2 text-white bg-black bg-opacity-50 rounded-full p-2">
+            <img src={ClickIcon} className="w-10 h-10" />
+          </div>
+        </div>
+        <Card.Body className="bg-[#272727] text-white font-[Inconsolata] font-extrabold text-xl">
+          {title}
+        </Card.Body>
+      </Card>
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        centered
+        dialogClassName="modal-custom"
+      >
+        <Modal.Body className="flex flex-col h-full bg-[#1e1e1e] p-0">
+          <Carousel
+            className="flex-grow w-full h-full"
+            indicators={false}
+            prevIcon={
+              <span className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-black/50 p-2 rounded-full text-white border-2 border-white">
+                &#10094;
+              </span>
+            }
+            nextIcon={
+              <span className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black/50 p-2 rounded-full text-white border-2 border-white">
+                &#10095;
+              </span>
+            }
+          >
+            {images.map((image, index) => (
+              <Carousel.Item key={index} className="h-full relative">
+                <img
+                  className="d-block w-full h-full object-cover"
+                  src={image.src}
+                  alt={`Slide ${index}`}
+                />
+                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+                  {images.map((_, i) => (
+                    <div
+                      key={i}
+                      className={`w-3 h-3 rounded-full cursor-pointer ${
+                        i === index ? "bg-white" : "bg-gray-500"
+                      }`}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    />
+                  ))}
+                </div>
+                <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 text-center text-white bg-black/50 px-4 pt-2 rounded">
+                  <p>{image.description}</p>
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default GalleryCard;
