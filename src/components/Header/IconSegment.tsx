23fa854b2fc92443d2db278bdbb17392004c2dad
itelet <szabo.adam.0725@gmail.com>
2025-01-31 13:10:51 +0100

    Add links to icons

--- src/components/Header/IconSegment.tsx
+++ src/components/Header/IconSegment.tsx
@@ -7,15 +7,34 @@ import EmailIcon from "../../assets/icons/email";
 const IconSegment = () => {
   return (
     <div className="flex flex-row mt-1">
-      <div className="w-12 h-12 rounded-xl">
-        <LinkedinIcon height={"100%"} width={"100%"} fillColor={"#5C6A81"} />
-      </div>
-      <div className="w-12 h-12 ml-3 rounded-xl">
-        <GithubIcon height={"100%"} width={"100%"} fillColor={"#B8B8B8"} />
-      </div>
-      <div className="w-12 h-12 ml-3 rounded-xl">
-        <EmailIcon height={"100%"} width={"100%"} fillColor={"#FF9B9B"} />
-      </div>
+      <a
+        href="https://www.linkedin.com/in/%C3%A1d%C3%A1m-szab%C3%B3-803297200/"
+        target="_blank"
+        rel="noopener noreferrer"
+				title="https://www.linkedin.com/in/%C3%A1d%C3%A1m-szab%C3%B3-803297200/"
+      >
+        <div className="w-12 h-12 rounded-xl">
+          <LinkedinIcon height={"100%"} width={"100%"} fillColor={"#5C6A81"} />
+        </div>
+      </a>
+      <a
+        href="https://github.com/itelet"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="https://github.com/itelet"
+      >
+        <div className="w-12 h-12 ml-3 rounded-xl">
+          <GithubIcon height={"100%"} width={"100%"} fillColor={"#B8B8B8"} />
+        </div>
+      </a>
+      <a
+        href="mailto:szabo.adam.0725@gmail.com"
+        title="szabo.adam.0725@gmail.com"
+      >
+        <div className="w-12 h-12 ml-3 rounded-xl">
+          <EmailIcon height={"100%"} width={"100%"} fillColor={"#FF9B9B"} />
+        </div>
+      </a>
     </div>
   );
 };
