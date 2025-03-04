3194a0300b0b78a6fd32d3d582a83dade5edbe62
itelet <szabo.adam.0725@gmail.com>
2025-03-04 19:14:15 +0100

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,24 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { ThemeProvider } from "react-bootstrap";
-import { About } from "./components/About/About";
-import { Experience } from "./components/Experience/Experience";
-import { Gallery } from "./components/Gallery/Gallery";
-import { Footer } from "./components/Footer/Footer";
 import { TypewriterProvider } from "./contexts/TypeWriterContext";
+import Navbar from "./components/Navbar/Navbar";
+import Line from "./components/utils/Line";
+import TopWrapper from "./components/App/TopWrapper";
 
 function App() {
   return (
     <TypewriterProvider>
-      <div className="App flex flex-col justify-center items-center">
+      <Navbar />
+      <TopWrapper>
         <ThemeProvider
           breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
           minBreakpoint="xxs"
         >
-          <div className="h-6" />
           <Header />
-					<div className="mt-4 bg-gradient-to-b from-[#1d1d1d] to-[#1a1a1a]">
-          <About />
-          <Experience />
-          <Gallery />
-					</div>
-          <Footer />
         </ThemeProvider>
-      </div>
+      </TopWrapper>
+      <Line />
     </TypewriterProvider>
   );
 }
--- src/components/Header/components/HeaderWrapper.tsx
+++ src/components/Header/components/HeaderWrapper.tsx
@@ -1,6 +1,6 @@
 const HeaderWrapper = ({ children }: { children: JSX.Element[] }) => {
   return (
-    <div className="sm:w-full md:w-full lg:w-[90%] xl:w-[75%]  self-center justify-center flex flex-row items-stretch mt-20 md:mt-28 mb-4">
+    <div className="sm:w-full md:w-full lg:w-[90%] xl:w-[75%]  self-center justify-center flex flex-row items-stretch mt-24 md:mt-28 mb-4">
       {children}
     </div>
   );
