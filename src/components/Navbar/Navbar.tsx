d518eba23cd32aa3716eca8e05c44af1fcb63789
itelet <szabo.adam.0725@gmail.com>
2025-03-04 18:55:54 +0100

    Add <Navbar />

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import LinkElement from "./components/LinkElement";
+import LinkWrapper from "./components/LinkWrapper";
+import NavWrapper from "./components/NavWrapper";
+
+const Navbar = () => {
+  return (
+    <NavWrapper>
+      <LinkWrapper>
+        <LinkElement href="#home" text="Home" />
+        <LinkElement href="#about" text="About" />
+        <LinkElement href="#projects" text="Projects" />
+        <LinkElement href="#contact" text="Contact" />
+      </LinkWrapper>
+    </NavWrapper>
+  );
+};
+
+export default Navbar;
