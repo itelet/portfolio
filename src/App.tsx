272ac566ec29fbe0c391880b052aba807bc6945c
itelet <szabo.adam.0725@gmail.com>
2025-03-05 14:44:22 +0100

    Fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,32 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Section";
 import { ThemeProvider } from "react-bootstrap";
 import { TypewriterProvider } from "./contexts/TypeWriterContext";
 import Navbar from "./components/Navbar/Navbar";
 import Line from "./components/utils/Line";
-import TopWrapper from "./components/lib/TopWrapper";
+import Header from "./components/Header/Section";
 import TechStackSection from "./components/TechStack/Section";
 import Switch from "./components/Switch/Section";
-import Projects from './components/Projects/Section';
-import Footer from './components/Footer/Section';
+import Projects from "./components/Projects/Section";
+import Footer from "./components/Footer/Section";
+import RotatingQuote from "./components/RotatingQuote/Section";
 
 function App() {
   return (
     <TypewriterProvider>
-      <Navbar />
-      <TopWrapper>
-        <ThemeProvider
-          breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-          minBreakpoint="xxs"
-        >
-          <Header />
-        </ThemeProvider>
-      </TopWrapper>
-      <Line />
-      <TopWrapper>
+      <ThemeProvider
+        breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+        minBreakpoint="xxs"
+      >
+				{/* <RotatingQuote /> */}
+        <Navbar />
+        <Header />
+        <Line />
         <TechStackSection />
-      </TopWrapper>
-			<TopWrapper>
-				<Switch />
-			</TopWrapper>
-			<TopWrapper>
-				<Projects />
-			</TopWrapper>
-			<TopWrapper>
-				<Footer />
-			</TopWrapper>
+        <Switch />
+        <Projects />
+        <Footer />
+      </ThemeProvider>
     </TypewriterProvider>
   );
 }
