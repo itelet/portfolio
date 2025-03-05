2d436780c1adbf13502869352a524640a0846859
itelet <szabo.adam.0725@gmail.com>
2025-03-05 01:05:33 +0100

    Add font-inter css imports

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,14 @@ import "@fontsource/inconsolata/700.css";
 import "@fontsource/inconsolata/800.css"; 
 import "@fontsource/inconsolata/900.css"; 
 import "@fontsource/inter";
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
