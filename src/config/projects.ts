450883fcd857b990cbda79d08c48ea33eced7a40
itelet <szabo.adam.0725@gmail.com>
2025-03-05 13:03:42 +0100

    Add config

---
+++ src/config/projects.ts
@@ -0,0 +1,33 @@
+import SolidTaste from '../assets/images/solid-taste.png'
+import RecipeParser from '../assets/images/recipe-parser.png'
+import Portfolio from '../assets/images/portfolio.png'
+import FreelanceWindow from '../assets/images/freelance-window.png'
+import FreelanceInsulation from '../assets/images/freelance-insulation.png'
+
+export default [
+  {
+		img: SolidTaste,
+    title: "Solid Taste",
+    description: "Solid Taste is a meal planner app featuring shopping list generation, dark/light mode, search, filtering, multilingual support, measurement unit selection, and more...",
+  },
+  {
+		img: Portfolio,
+    title: "Portfolio",
+    description: "This website was created using React with tailwind styling. The source code is available on github.",
+  },
+  {
+		img: FreelanceWindow,
+    title: "Price calculator",
+    description: "Freelance project done for a window installing company. It’s integrated into a WP + Elementor website via <Custom Code> and PHP hooks.",
+  },
+  {
+		img: FreelanceInsulation,
+    title: "Price calculator",
+    description: "Freelance project done for an insulation installing company. It’s integrated into a WP + Elementor website via <Custom Code> and PHP hooks.",
+  },
+	{
+		img: RecipeParser,
+		title: "Recipe parser",
+		description: "Standalone Electron app with an automatic recipe parsing script. It can scrape ingredient/unit pairs from cooking steps, also has Google Translate and deepL translation support.",
+	},
+];
