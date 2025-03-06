a826e24ceef542abb8bb3511b9b03100d6032b4c
itelet <szabo.adam.0725@gmail.com>
2025-03-06 21:03:36 +0100

    Restructure App, add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import { ThemeProvider } from "react-bootstrap";
 import { TypewriterProvider } from "./contexts/TypeWriterContext";
+import { Route, Switch } from "wouter";
 import Navbar from "./components/Navbar/Navbar";
-import Line from "./components/utils/Line";
-import Header from "./components/Header/Section";
-import TechStackSection from "./components/TechStack/Section";
-import Switch from "./components/Switch/Section";
-import Projects from "./components/Projects/Section";
 import Footer from "./components/Footer/Section";
-import RotatingQuote from "./components/RotatingQuote/Section";
+import Home from "./pages/Home";
+import About from "./pages/About/Section";
+import Projects from './pages/Projects/Projects';
+
+import Project from './pages/Project/Section';
 
 function App() {
   return (
@@ -18,14 +18,16 @@ function App() {
         breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
         minBreakpoint="xxs"
       >
-				{/* <RotatingQuote /> */}
+        {/* <RotatingQuote /> */}
         <Navbar />
-        <Header />
-        <Line />
-        <TechStackSection />
-        <Switch />
-        <Projects />
-        <Footer />
+
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/projects/:name" component={Project} />
+        </Switch>
+        {/* <Footer /> */}
       </ThemeProvider>
     </TypewriterProvider>
   );
