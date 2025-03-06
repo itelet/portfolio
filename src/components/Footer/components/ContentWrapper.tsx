6c975390adad825cd91cd3ae226a535a0a1a0cfc
itelet <szabo.adam.0725@gmail.com>
2025-03-06 21:01:32 +0100

    Modify <Footer />
    its still not working fine

--- src/components/Footer/components/ContentWrapper.tsx
+++ src/components/Footer/components/ContentWrapper.tsx
@@ -2,23 +2,22 @@ const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       className="
-        flex 
-        flex-col 
-        sm:flex-row 
-        justify-between 
-        w-full 
-        bg-[#131313] 
-        p-4 
-        px-6 
-        text-[#9A9A9A] 
-        font-[Inter] 
-        font-light 
-        rounded-t-lg 
-        items-center 
-        sm:items-center 
-        gap-2 
-        sm:gap-0
-      "
+    flex 
+    flex-col 
+    sm:flex-row 
+    justify-between 
+    bg-[#131313] 
+    p-4 
+    px-6 
+    text-[#9A9A9A] 
+    font-[Inter] 
+    font-light 
+    rounded-t-lg 
+    items-center 
+    gap-2 
+    sm:gap-0 
+    mt-auto
+  "
     >
       {children}
     </div>
