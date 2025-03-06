15374f7e9eedd4ef510be02eb0cc0899216fa327
itelet <szabo.adam.0725@gmail.com>
2025-03-06 21:01:53 +0100

    Move <Title /> to reusable

--- src/components/lib/Title.tsx
+++ src/components/lib/Title.tsx
@@ -1,5 +1,11 @@
 const Title = ({ text }: { text: string }) => {
-  return <span className="font-[Inter] font-light text-4xl text-white self-start mb-4">{text}</span>;
+  return (
+    <span
+      className={`font-[Inter] font-light text-4xl text-white self-start mb-4`}
+    >
+      {text}
+    </span>
+  );
 };
 
 export default Title;
