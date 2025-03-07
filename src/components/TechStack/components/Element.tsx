53333221afc4ec62107ba203e27e6815ea4c8588
itelet <szabo.adam.0725@gmail.com>
2025-03-07 02:07:12 +0100

    Fix icon size bug

--- src/components/TechStack/components/Element.tsx
+++ src/components/TechStack/components/Element.tsx
@@ -10,7 +10,7 @@ const Element = ({
   return (
     <div className="bg-[#363636] rounded-lg items-center flex flex-row mr-2 mb-1.5">
       <div className="h-8 sm:h-10">
-        <StackIcon name={selector} className="h-8 sm:h-10 p-1.5" />
+        <StackIcon name={selector} className="h-8 sm:h-10 max-h-10 max-w-10 p-1.5" />
       </div>
       <span className="pl-1.5 pr-2 font-[Inter] text-white font-thin text-sm sm:text-lg">
         {title}
