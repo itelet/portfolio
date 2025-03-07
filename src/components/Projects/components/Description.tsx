585e2c3532d0b23c6f0616763b57cb2882ec4d1d
itelet <szabo.adam.0725@gmail.com>
2025-03-07 01:55:04 +0100

    Add missing style

--- src/components/Projects/components/Description.tsx
+++ src/components/Projects/components/Description.tsx
@@ -1,6 +1,6 @@
 const Description = ({ description }: { description: string }) => {
   return (
-    <span className="font-[Inter] text-[#9A9A9A] text-xs font-normal text-left px-6 mt-2.5 pb-3.5">
+    <span className="font-[Inter] text-[#9A9A9A] text-xs font-normal text-left px-6 mt-2.5 pb-3.5 sm:text-sm">
       {description}
     </span>
   );
